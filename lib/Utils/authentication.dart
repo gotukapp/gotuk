ffab113e61869b5b881f4bc90e50516e42bf7a33
jpenas <jppenas@gmail.com>
2025-04-05 00:01:35 +0100

    translations

--- lib/Utils/authentication.dart
+++ lib/Utils/authentication.dart
@@ -2,14 +2,13 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 
-import '../CreatAccount/verifyaccount.dart';
+import '../CreatAccount/verifyAccount.dart';
 
 Future<UserCredential?> signInWithPhoneNumber(BuildContext context, String phoneNumber, Function callback) async {
   UserCredential? userCredential;
   await FirebaseAuth.instance.verifyPhoneNumber(
     phoneNumber: phoneNumber,
     verificationCompleted: (PhoneAuthCredential credential) async {
-      // This callback is triggered when verification is completed automatically
       userCredential = await FirebaseAuth.instance.signInWithCredential(credential);
       print("User signed in automatically with phone credentials.");
     },
@@ -28,7 +27,7 @@ Future<UserCredential?> signInWithPhoneNumber(BuildContext context, String phone
       print("Verification code sent to $phoneNumber.");
 
       String smsCode = await Navigator.of(context).push(MaterialPageRoute(
-          builder: (context) => const verifyaccount()));;
+          builder: (context) => const verifyAccount()));
 
       // Create a PhoneAuthCredential using the code and the verificationId
       PhoneAuthCredential credential = PhoneAuthProvider.credential(
